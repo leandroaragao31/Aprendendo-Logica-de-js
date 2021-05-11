447a0727a1710c83086bbf990fe34ef355bb0c7d
leandroaragao31 <leandroaragao31@gmail.com>
2021-05-11 11:04:12 -0300

    criando variaveis , atribuindo valor boleano, e subtraindo e somando essas variáveis

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Leandro"
+let age = 31
+let year = 2021
+
+console.log("meu ano de Nascimento é", year - age)
+
+let iamHuman = true
+
+let number = 37
+let number2 = 28
+
+console.log("a somba entre os numeros é", number + number2)
